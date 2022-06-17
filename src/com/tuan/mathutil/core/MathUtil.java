e4c025515065813335cb6ca37f640bfe3275ad45
TuanPA0212 <phananhtuan439@gmail.com>
2022-06-17 10:03:16 +0700

    refactor the main code and green 4ever always

--- src/com/tuan/mathutil/core/MathUtil.java
+++ src/com/tuan/mathutil/core/MathUtil.java
@@ -23,15 +23,42 @@ public class MathUtil {
     //21 giai thừa tràn kiểu long
     //bài này ước tính n! trong khoảng từ 0 - 20
     
+//    public static long getFactorial(int n){
+//        if (n < 0 || n >20)
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0 .. 20");
+//        if (n == 0 || n == 1)
+//            return 1; //kết thúc sớm nếu nhận những đầu vào đặc biệt
+//        long product = 1; //tích nhân dồn, thuật toán heo đất
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }       
+    
+    //Học lại đệ quy
+    //Hiện tượng gọi lại chính mình vs 1 quy mô khác
+    //vd: con búp bê Nga
+    //búp bê to, nhỏ hơn, nhỏ hơn nữa ...
+    
+    //tính giùm 6!
+    //n! = 1.2.3.4.5.6 ... .n
+    //6! = 6 x 5!
+    //5! = 5 x 4!
+    //4! = 4 x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1! //điểm dừng
+    //1! = 1
+    //quy ước 1! = 0! = 1;
+    //ct n* (n-1)
+    
     public static long getFactorial(int n){
-        if (n < 0 || n >20)
-            throw new IllegalArgumentException("Invalid argument. N must be between 0 .. 20");
-        if (n == 0 || n == 1)
+     if(n < 0 || n > 20)
+         throw new IllegalArgumentException("n must between  0..20 plz");
+     if (n == 0 || n == 1)
             return 1; //kết thúc sớm nếu nhận những đầu vào đặc biệt
-        long product = 1; //tích nhân dồn, thuật toán heo đất
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
-    }       
+        
+        return n* getFactorial(n-1); //ct đệ quy
+    } 
+    
+    
 }
